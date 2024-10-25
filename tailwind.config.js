593274e592a6b3fed22ad04249102bb492393b6c
Viraj-2000 <151899413+Viraj-2000@users.noreply.github.com>
2024-10-25 15:02:36 +0530

    Starter UI -viraj

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-bg": "#0a0a23", // Background to match your screenshot
+        "purple-gradient":
+          "linear-gradient(to right, #0F0C29, #302B63, #24243E)",
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
-}
-
+  plugins: [flowbite.plugin()],
+};
