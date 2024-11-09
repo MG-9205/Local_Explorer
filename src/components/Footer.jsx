dad9de87e5f2339db2db20ced999849319fb4f8d
Manish Gupta <manishgupta9205@gmail.com>
2024-11-09 19:50:05 +0530

    adding clerk authencation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="text-white py-8">
+    <footer className="text-white py-8 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Logo and Short Description */}
